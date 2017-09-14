9aefb17c4879ade4164fdc7b5ae130880eb06e9d
IsaiahLamas <31754821+IsaiahLamas@users.noreply.github.com>
2017-09-13 21:21:02 -0700

    My First Homework Assignment

---
+++ Homework+1.py
@@ -0,0 +1,112 @@
+
+# coding: utf-8
+
+# In[1]:
+
+x=5
+y=10
+print x,y
+
+
+# In[2]:
+
+x=10
+y=20
+z= x + y
+print z
+
+
+
+# In[3]:
+
+a= 5
+b= 6
+print a+ b
+
+
+# In[2]:
+
+greeting= "Hello"
+person= "Oscar"
+
+
+# In[3]:
+
+personalGreeting= greeting+" " + person
+print personalGreeting
+
+
+# In[14]:
+
+print('enter your age:')
+age=int(input())
+print(age)
+if age==35:
+    print ("age is equal to 35")
+if age>35:
+    print ("age is greater than 35")  
+if age<35:
+    print ("age is less than 35")    
+
+
+# In[1]:
+
+x=10
+print x
+
+
+# In[2]:
+
+y=x/4
+print x
+
+
+# In[10]:
+
+y=15
+y=y//4
+print y
+
+
+# In[13]:
+
+z=20
+z=z%4
+print z
+
+
+# In[18]:
+
+x=101.35
+x=x/4
+x=x//4
+x=x%4
+print x
+
+
+# In[32]:
+
+myage=21
+yourage=25
+if (myage)==(yourage):
+    print ("ages are equal")
+else:
+    print ("ages are not equal")
+
+    
+
+
+# In[33]:
+
+myage=21
+hisage=25
+if (myage)==(hisage):
+    print ("ages are equal")
+else:
+    print ("ages are not equal")
+
+
+# In[ ]:
+
+
+
